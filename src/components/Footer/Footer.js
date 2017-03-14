8008cbcb8c9a4ae93069d82cfd7e602cd828acf2
chris <chris@readytouch.com>
2017-03-13 18:20:36 -0700

    testing yelp api and translate footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
+var Yelp = require('yelp');
+
+var yelp = new Yelp({
+  consumer_key: '53Fk-70GCjVzZVyXEH0avQ',
+  consumer_secret: 'Qvmca553bAh2wnfHt8hla6jr4Ls',
+  token: '2g0TC-RTlGlvikrZ4qesIKtcXMosObJK',
+  token_secret: 'ebhDU5oujyIcq3TxlXusvGqw9JA',
+});
+
+function Welcome() {
+  yelp.search({ term: 'food', location: 'Montreal' })
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
+
 
 class Footer extends Component {
+
+  componentDidMount(){
+    Welcome()
+  }
+
+
+
+  // var test = function(){
+  //
+  // }
+
+
   render() {
+    let content = this.props.content;
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <h2 className="primary-title" style={{"color":"#d32323"}}>FIND US ON YELP</h2>
+            <h2 className="primary-title" style={{"color":"#d32323"}}>{content.footer.findusonyelp}</h2>
             <img className="col-xs-12 col-md-12" src="/o.jpg" role="presentation"></img>
-            <p><a className="btn btn-danger" href="https://www.yelp.com/biz/bonjour-professional-iphone-ipad-repair-and-electronic-center-san-francisco?adjust_creative=hWC8vHA1xGldI4IJnfcLKQ&utm_campaign=yelp_api&utm_medium=api_v2_business&utm_source=hWC8vHA1xGldI4IJnfcLKQ" role="button">View details »</a></p>
+            <p><a className="btn btn-danger" href="https://www.yelp.com/biz/bonjour-professional-iphone-ipad-repair-and-electronic-center-san-francisco?adjust_creative=hWC8vHA1xGldI4IJnfcLKQ&utm_campaign=yelp_api&utm_medium=api_v2_business&utm_source=hWC8vHA1xGldI4IJnfcLKQ" role="button">{content.footer.detail} »</a></p>
           </div>
           <div className="col-md-4">
-            <h2 style={{"color":"#619de5"}}>MAP</h2>
+            <h2 style={{"color":"#619de5"}}>{content.footer.map}</h2>
             <iframe className="col-xs-12 col-md-12" height="300" frameBorder="0" style={{border:"0"}} src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJUSGv8WWHhYARMREszDov4lM&key=AIzaSyBIphdq8aiWRDeafRSJ12WV2wpi0H5-Zbw" allowFullScreen></iframe>
           </div>
           <div className="col-md-4">
-            <h2 style={{"color":"rgb(101, 158, 203)"}}>FREE PARKING</h2>
+            <h2 style={{"color":"rgb(101, 158, 203)"}}>{content.footer.freeparking}</h2>
             <img className="col-xs-12 col-sm-12 col-md-12" src="/download.jpeg" role="presentation"></img>
-            <p><Link to={'/parking'}><button className="btn btn-primary" role="button">View details »</button></Link></p>
+            <p><Link to={'/parking'}><button className="btn btn-primary" role="button">{content.footer.detail} »</button></Link></p>
           </div>
         </div>
         <hr/>
