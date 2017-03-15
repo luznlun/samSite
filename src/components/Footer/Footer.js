2ec056a479e6d13f7e72205c293311cf39a51570
chris <chris@readytouch.com>
2017-03-14 23:47:39 -0700

    finish yelp api

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,95 +1,94 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
-import $ from 'jquery';
-
 var oauthSignature = require('oauth-signature');
 var n = require('nonce')();
 var request = require('request');
 var qs = require('querystring');
 var _ = require('lodash');
 
-/* Function for yelp call
- * ------------------------
- * set_parameters: object with params to search
- * callback: callback(error, response, body)
- */
-var request_yelp = function(set_parameters, callback) {
-
-  /* The type of request */
-  var httpMethod = 'GET';
-
-  var url = 'http://api.yelp.com/v2/business/bonjour-professional-iphone-ipad-repair-and-electronic-center-san-francisco';
-
-    /* We can setup default parameters here */
-    var default_parameters = {
-    };
-
-  /* We set the require parameters here */
-  var required_parameters = {
-    oauth_consumer_key : '53Fk-70GCjVzZVyXEH0avQ',
-    oauth_token : 'Tfx-YcWPXNeME7AF5Wsdeq6lvPIRugBv',
-    oauth_nonce : n(),
-    oauth_timestamp : n().toString().substr(0,10),
-    oauth_signature_method : 'HMAC-SHA1',
-    oauth_version : '1.0'
-  };
-
-  /* We combine all the parameters in order of importance */
-  var parameters = _.assign(default_parameters, set_parameters, required_parameters);
-
-  /* We set our secrets here */
-  var consumerSecret = 'Qvmca553bAh2wnfHt8hla6jr4Ls';
-  var tokenSecret = 'sUIlbk7wEe_q6IMgizCVO6myjs8';
-
-  /* Then we call Yelp's Oauth 1.0a server, and it returns a signature */
-  /* Note: This signature is only good for 300 seconds after the oauth_timestamp */
-  var signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
-
-  /* We add the signature to the list of paramters */
-  parameters.oauth_signature = signature;
-
-  /* Then we turn the paramters object, to a query string */
-  var paramURL = qs.stringify(parameters);
-
-  /* Add the query string to the url */
-  var apiURL = url+'?'+paramURL;
-
-  /* Then we use request to send make the API Request */
-  request(apiURL, function(error, response, body){
-    return callback(error, response, body);
-  });
-
-};
-
-var callback = function(error, response, body){
-  console.log(response);
-}
-
-
-
-function Welcome(){
-  request_yelp({cc:"US"}, callback);
-}
-
 
 class Footer extends Component {
 
-  componentDidMount(){
-    Welcome()
+  constructor(props) {
+    super(props);
+    this.state = {body: ""};
   }
+
+  componentDidMount(){
+    this.Welcome();
+  }
+
+  callback(error, response, body){
+    this.setState({body: JSON.parse(body)});
+  }
+
+  Welcome(){
+     this.request_yelp({cc:"US"}, this.callback.bind(this));
+  }
+
+  request_yelp(set_parameters, callback) {
+
+    /* The type of request */
+    var httpMethod = 'GET';
+
+    var url = 'http://api.yelp.com/v2/business/bonjour-professional-iphone-ipad-repair-and-electronic-center-san-francisco';
+
+      /* We can setup default parameters here */
+      var default_parameters = {
+      };
+
+    /* We set the require parameters here */
+    var required_parameters = {
+      oauth_consumer_key : '53Fk-70GCjVzZVyXEH0avQ',
+      oauth_token : 'Tfx-YcWPXNeME7AF5Wsdeq6lvPIRugBv',
+      oauth_nonce : n(),
+      oauth_timestamp : n().toString().substr(0,10),
+      oauth_signature_method : 'HMAC-SHA1',
+      oauth_version : '1.0'
+    };
+
+    /* We combine all the parameters in order of importance */
+    var parameters = _.assign(default_parameters, set_parameters, required_parameters);
+
+    /* We set our secrets here */
+    var consumerSecret = 'Qvmca553bAh2wnfHt8hla6jr4Ls';
+    var tokenSecret = 'sUIlbk7wEe_q6IMgizCVO6myjs8';
+
+    /* Then we call Yelp's Oauth 1.0a server, and it returns a signature */
+    /* Note: This signature is only good for 300 seconds after the oauth_timestamp */
+    var signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
+
+    /* We add the signature to the list of paramters */
+    parameters.oauth_signature = signature;
+
+    /* Then we turn the paramters object, to a query string */
+    var paramURL = qs.stringify(parameters);
+
+    /* Add the query string to the url */
+    var apiURL = url+'?'+paramURL;
+
+    /* Then we use request to send make the API Request */
+    request(apiURL, function(error, response, body){
+      return callback(error, response, body);
+    });
+
+  };
 
 
 
 
   render() {
     let content = this.props.content;
+    console.log("***body: ",this.state.body.snippet_image_url)
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-4">
             <h2 className="primary-title" style={{"color":"#d32323"}}>{content.footer.findusonyelp}</h2>
-            <img className="col-xs-12 col-md-12" src="/o.jpg" role="presentation"></img>
-            <p><a className="btn btn-danger" href="https://www.yelp.com/biz/bonjour-professional-iphone-ipad-repair-and-electronic-center-san-francisco?adjust_creative=hWC8vHA1xGldI4IJnfcLKQ&utm_campaign=yelp_api&utm_medium=api_v2_business&utm_source=hWC8vHA1xGldI4IJnfcLKQ" role="button">{content.footer.detail} »</a></p>
+            <img className="col-xs-12 col-md-12" src={this.state.body.image_url} role="presentation"></img>
+            <div className="container">
+              <img src={this.state.body.rating_img_url} role="presentation"></img><span>reviews: {this.state.body.review_count}</span>
+            </div>
           </div>
           <div className="col-md-4">
             <h2 style={{"color":"#619de5"}}>{content.footer.map}</h2>
